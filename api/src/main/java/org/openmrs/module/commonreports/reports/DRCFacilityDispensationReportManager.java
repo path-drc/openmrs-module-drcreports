cfff3a13f89223ef9cdeb1c97e627dc08c33e91b
MAKOBA REAGAN PATRICK <47450907+reagan-meant@users.noreply.github.com>
2025-05-20 18:37:42 +0300

    (feat) Add Facility Based Dispensation Report (#10)

--- api/src/main/java/org/openmrs/module/commonreports/reports/DRCFacilityDispensationReportManager.java
+++ api/src/main/java/org/openmrs/module/commonreports/reports/DRCFacilityDispensationReportManager.java
@@ -43,7 +43,7 @@ import org.openmrs.module.reporting.cohort.definition.AgeCohortDefinition;
 import org.openmrs.module.reporting.common.DateUtil;
 
 @Component
-public class DRCFacilityBasedARTDeliveryReportManager extends ActivatedReportManager {
+public class DRCFacilityDispensationReportManager extends ActivatedReportManager {
 	
 	@Autowired
 	private InitializerService inizService;
@@ -62,23 +62,25 @@ public class DRCFacilityBasedARTDeliveryReportManager extends ActivatedReportMan
 	
 	@Override
 	public String getUuid() {
-		return "f6b09a7e-f330-431d-b7b8-891dd35db0e1";
+		return "4253d3b2-f108-4672-a2a6-b7eb67a51a10";
 	}
 	
 	@Override
 	public String getName() {
-		return MessageUtil.translate("commonreports.report.drc.facilityBasedARTDelivery.reportName");
+		return MessageUtil.translate("commonreports.report.drc.facilityDispensation.reportName");
 	}
 	
 	@Override
 	public String getDescription() {
-		return MessageUtil.translate("commonreports.report.drc.facilityBasedARTDelivery.reportDescription");
+		return MessageUtil.translate("commonreports.report.drc.facilityDispensation.reportDescription");
 	}
 	
-	private Parameter getReportingDateParameter() {
-		String today = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-		return new Parameter("onOrBefore", MessageUtil.translate("commonreports.report.util.reportingEndDate"), Date.class,
-		        null, DateUtil.parseDate(today, "yyyy-MM-dd"));
+	private Parameter getStartDateParameter() {
+		return new Parameter("startDate", MessageUtil.translate("commonreports.report.util.reportingStartDate"), Date.class);
+	}
+	
+	private Parameter getEndDateParameter() {
+		return new Parameter("endDate", MessageUtil.translate("commonreports.report.util.reportingEndDate"), Date.class);
 	}
 	
 	public static String col1 = "";
@@ -106,8 +108,8 @@ public class DRCFacilityBasedARTDeliveryReportManager extends ActivatedReportMan
 	@Override
 	public List<Parameter> getParameters() {
 		List<Parameter> params = new ArrayList<Parameter>();
-		params.add(getReportingDateParameter());
-		
+		params.add(getStartDateParameter());
+		params.add(getEndDateParameter());
 		return params;
 	}
 	
@@ -119,50 +121,77 @@ public class DRCFacilityBasedARTDeliveryReportManager extends ActivatedReportMan
 		rd.setDescription(getDescription());
 		rd.setParameters(getParameters());
 		
-		// facilityBasedARTDelivery Grouping
-		CohortCrossTabDataSetDefinition facilityBasedARTDelivery = new CohortCrossTabDataSetDefinition();
-		facilityBasedARTDelivery.addParameters(getParameters());
-		rd.addDataSetDefinition(getName(), Mapped.mapStraightThrough(facilityBasedARTDelivery));
+		// facilityDispensation Grouping
+		CohortCrossTabDataSetDefinition facilityDispensation = new CohortCrossTabDataSetDefinition();
+		facilityDispensation.addParameters(getParameters());
+		rd.addDataSetDefinition(getName(), Mapped.mapStraightThrough(facilityDispensation));
 		
 		Map<String, Object> parameterMappings = new HashMap<String, Object>();
-		parameterMappings.put("onOrBefore", "${onOrBefore}");
+		parameterMappings.put("onOrAfter", "${startDate}");
+		parameterMappings.put("onOrBefore", "${endDate}");
 		
+		// ART refill for <90 days(< 3 months) with reporting in range
 		SqlCohortDefinition sqd = new SqlCohortDefinition();
-		
-		// ART plan -> Started drugs
-		// Visit in past 90 days
-		// ART refill model -> Normal with visit
-		// Not transferred
 		String sql = "SELECT DISTINCT p.patient_id FROM patient p WHERE p.voided = 0 "
-		        + "AND EXISTS (SELECT 1 FROM obs o JOIN concept c_question ON o.concept_id = c_question.concept_id JOIN concept c_answer ON o.value_coded = c_answer.concept_id WHERE o.person_id = p.patient_id AND c_question.uuid = '1255AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' AND c_answer.uuid = '1256AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' AND o.voided = 0) "
-		        + "AND EXISTS (SELECT 1 FROM visit v WHERE v.patient_id = p.patient_id AND v.date_started BETWEEN DATE_SUB(:onOrBefore, INTERVAL 90 DAY) AND :onOrBefore AND v.voided = 0) "
-		        + "AND EXISTS (SELECT 1 FROM obs o2 JOIN concept cq2 ON o2.concept_id = cq2.concept_id JOIN concept ca2 ON o2.value_coded = ca2.concept_id WHERE o2.person_id = p.patient_id AND cq2.uuid = '166448AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' AND ca2.uuid = '166447AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' AND o2.voided = 0 AND o2.obs_datetime = (SELECT MAX(o3.obs_datetime) FROM obs o3 WHERE o3.person_id = p.patient_id AND o3.concept_id = cq2.concept_id AND o3.voided = 0)) "
-		        + "AND NOT EXISTS (SELECT 1 FROM obs o4 JOIN concept cq4 ON o4.concept_id = cq4.concept_id JOIN concept ca4 ON o4.value_coded = ca4.concept_id WHERE o4.person_id = p.patient_id AND cq4.uuid = '797e0073-1f3f-46b1-8b1a-8cdad134d2b3' AND ca4.uuid = '1065AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' AND o4.voided = 0 AND o4.obs_datetime = (SELECT MAX(o5.obs_datetime) FROM obs o5 WHERE o5.person_id = p.patient_id AND o5.concept_id = cq4.concept_id AND o5.voided = 0));";
+		        + "AND EXISTS (SELECT 1 FROM obs o_num JOIN concept c_num ON o_num.concept_id = c_num.concept_id WHERE o_num.person_id = p.patient_id AND c_num.uuid = '3a0709e9-d7a8-44b9-9512-111db5ce3989' AND o_num.voided = 0 AND o_num.value_numeric < 90 AND o_num.obs_datetime BETWEEN :onOrAfter AND :onOrBefore); ";
 		sqd.setQuery(sql);
+		sqd.addParameter(new Parameter("onOrAfter", "On Or After", Date.class));
 		sqd.addParameter(new Parameter("onOrBefore", "On Or Before", Date.class));
 		
-		// Alive patients
-		BirthAndDeathCohortDefinition livePatients = new BirthAndDeathCohortDefinition();
-		livePatients.setDied(false);
-		
 		CompositionCohortDefinition ccd = new CompositionCohortDefinition();
-		ccd.initializeFromElements(sqd, livePatients);
-		facilityBasedARTDelivery.addRow(getName(), ccd, parameterMappings);
+		ccd.initializeFromElements(sqd);
+		
+		// ART refill for 90-149 days(3-5 months) with reporting in range
+		SqlCohortDefinition sqd2 = new SqlCohortDefinition();
+		String sql2 = "SELECT DISTINCT p.patient_id FROM patient p WHERE p.voided = 0 "
+		        + "AND EXISTS (SELECT 1 FROM obs o_num JOIN concept c_num ON o_num.concept_id = c_num.concept_id "
+		        + "WHERE o_num.person_id = p.patient_id " + "AND c_num.uuid = '3a0709e9-d7a8-44b9-9512-111db5ce3989' "
+		        + "AND o_num.voided = 0 " + "AND o_num.value_numeric >= 90 AND o_num.value_numeric <= 149 "
+		        + "AND o_num.obs_datetime BETWEEN :onOrAfter AND :onOrBefore); ";
+		
+		sqd2.setQuery(sql2);
+		sqd2.addParameter(new Parameter("onOrAfter", "On Or After", Date.class));
+		sqd2.addParameter(new Parameter("onOrBefore", "On Or Before", Date.class));
+		
+		CompositionCohortDefinition ccd2 = new CompositionCohortDefinition();
+		ccd2.initializeFromElements(sqd2);
+		
+		// ART refill for >=150 days(>5months) with reporting in range
+		SqlCohortDefinition sqd3 = new SqlCohortDefinition();
+		String sql3 = "SELECT DISTINCT p.patient_id FROM patient p WHERE p.voided = 0 "
+		        + "AND EXISTS (SELECT 1 FROM obs o_num JOIN concept c_num ON o_num.concept_id = c_num.concept_id "
+		        + "WHERE o_num.person_id = p.patient_id " + "AND c_num.uuid = '3a0709e9-d7a8-44b9-9512-111db5ce3989' "
+		        + "AND o_num.voided = 0 " + "AND o_num.value_numeric >= 150 "
+		        + "AND o_num.obs_datetime BETWEEN :onOrAfter AND :onOrBefore); ";
+		
+		sqd3.setQuery(sql3);
+		sqd3.addParameter(new Parameter("onOrAfter", "On Or After", Date.class));
+		sqd3.addParameter(new Parameter("onOrBefore", "On Or Before", Date.class));
+		
+		CompositionCohortDefinition ccd3 = new CompositionCohortDefinition();
+		ccd3.initializeFromElements(sqd3);
+		
+		facilityDispensation.addRow(MessageUtil.translate("commonreports.report.drc.facilityDispensationBelow3months"), ccd,
+		    parameterMappings);
+		facilityDispensation.addRow(MessageUtil.translate("commonreports.report.drc.facilityDispensation3To5months"), ccd2,
+		    parameterMappings);
+		facilityDispensation.addRow(MessageUtil.translate("commonreports.report.drc.facilityDispensationAbove5months"), ccd3,
+		    parameterMappings);
 		
 		setColumnNames();
 		
 		GenderCohortDefinition males = new GenderCohortDefinition();
 		males.setMaleIncluded(true);
-		facilityBasedARTDelivery.addColumn(col1, createCohortComposition(males), null);
+		facilityDispensation.addColumn(col1, createCohortComposition(males), null);
 		
 		GenderCohortDefinition females = new GenderCohortDefinition();
 		females.setFemaleIncluded(true);
-		facilityBasedARTDelivery.addColumn(col2, createCohortComposition(females), null);
+		facilityDispensation.addColumn(col2, createCohortComposition(females), null);
 		
 		GenderCohortDefinition allGenders = new GenderCohortDefinition();
 		allGenders.setFemaleIncluded(true);
 		allGenders.setMaleIncluded(true);
-		facilityBasedARTDelivery.addColumn(col3, createCohortComposition(allGenders), null);
+		facilityDispensation.addColumn(col3, createCohortComposition(allGenders), null);
 		
 		// < 1 year
 		AgeCohortDefinition under1y = new AgeCohortDefinition();
@@ -171,7 +200,7 @@ public class DRCFacilityBasedARTDeliveryReportManager extends ActivatedReportMan
 		under1y.setMaxAge(11);
 		under1y.setMaxAgeUnit(DurationUnit.MONTHS);
 		under1y.addParameter(new Parameter("effectiveDate", "Effective Date", Date.class));
-		facilityBasedARTDelivery.addColumn(col4, createCohortComposition(under1y), null);
+		facilityDispensation.addColumn(col4, createCohortComposition(under1y), null);
 		
 		// 1-4 years
 		AgeCohortDefinition _1To4y = new AgeCohortDefinition();
@@ -180,7 +209,7 @@ public class DRCFacilityBasedARTDeliveryReportManager extends ActivatedReportMan
 		_1To4y.setMaxAge(4);
 		_1To4y.setMaxAgeUnit(DurationUnit.YEARS);
 		_1To4y.addParameter(new Parameter("effectiveDate", "Effective Date", Date.class));
-		facilityBasedARTDelivery.addColumn(col5, createCohortComposition(_1To4y), null);
+		facilityDispensation.addColumn(col5, createCohortComposition(_1To4y), null);
 		
 		// 5-9 years
 		AgeCohortDefinition _5To9y = new AgeCohortDefinition();
@@ -189,7 +218,7 @@ public class DRCFacilityBasedARTDeliveryReportManager extends ActivatedReportMan
 		_5To9y.setMaxAge(9);
 		_5To9y.setMaxAgeUnit(DurationUnit.YEARS);
 		_5To9y.addParameter(new Parameter("effectiveDate", "Effective Date", Date.class));
-		facilityBasedARTDelivery.addColumn(col6, createCohortComposition(_5To9y), null);
+		facilityDispensation.addColumn(col6, createCohortComposition(_5To9y), null);
 		
 		// 10-14 years
 		AgeCohortDefinition _10To14y = new AgeCohortDefinition();
@@ -198,7 +227,7 @@ public class DRCFacilityBasedARTDeliveryReportManager extends ActivatedReportMan
 		_10To14y.setMaxAge(14);
 		_10To14y.setMaxAgeUnit(DurationUnit.YEARS);
 		_10To14y.addParameter(new Parameter("effectiveDate", "Effective Date", Date.class));
-		facilityBasedARTDelivery.addColumn(col7, createCohortComposition(_10To14y), null);
+		facilityDispensation.addColumn(col7, createCohortComposition(_10To14y), null);
 		
 		// 15-19 years
 		AgeCohortDefinition _15To19y = new AgeCohortDefinition();
@@ -207,7 +236,7 @@ public class DRCFacilityBasedARTDeliveryReportManager extends ActivatedReportMan
 		_15To19y.setMaxAge(19);
 		_15To19y.setMaxAgeUnit(DurationUnit.YEARS);
 		_15To19y.addParameter(new Parameter("effectiveDate", "Effective Date", Date.class));
-		facilityBasedARTDelivery.addColumn(col8, createCohortComposition(_15To19y), null);
+		facilityDispensation.addColumn(col8, createCohortComposition(_15To19y), null);
 		
 		// 20-24 years
 		AgeCohortDefinition _20To24y = new AgeCohortDefinition();
@@ -216,7 +245,7 @@ public class DRCFacilityBasedARTDeliveryReportManager extends ActivatedReportMan
 		_20To24y.setMaxAge(24);
 		_20To24y.setMaxAgeUnit(DurationUnit.YEARS);
 		_20To24y.addParameter(new Parameter("effectiveDate", "Effective Date", Date.class));
-		facilityBasedARTDelivery.addColumn(col9, createCohortComposition(_20To24y), null);
+		facilityDispensation.addColumn(col9, createCohortComposition(_20To24y), null);
 		
 		// 25-49 years
 		AgeCohortDefinition _25To49y = new AgeCohortDefinition();
@@ -225,7 +254,7 @@ public class DRCFacilityBasedARTDeliveryReportManager extends ActivatedReportMan
 		_25To49y.setMaxAge(49);
 		_25To49y.setMaxAgeUnit(DurationUnit.YEARS);
 		_25To49y.addParameter(new Parameter("effectiveDate", "Effective Date", Date.class));
-		facilityBasedARTDelivery.addColumn(col10, createCohortComposition(_25To49y), null);
+		facilityDispensation.addColumn(col10, createCohortComposition(_25To49y), null);
 		
 		// 50+ years
 		AgeCohortDefinition _50andAbove = new AgeCohortDefinition();
@@ -234,7 +263,7 @@ public class DRCFacilityBasedARTDeliveryReportManager extends ActivatedReportMan
 		_50andAbove.setMaxAge(200);
 		_50andAbove.setMaxAgeUnit(DurationUnit.YEARS);
 		_50andAbove.addParameter(new Parameter("effectiveDate", "Effective Date", Date.class));
-		facilityBasedARTDelivery.addColumn(col11, createCohortComposition(_50andAbove), null);
+		facilityDispensation.addColumn(col11, createCohortComposition(_50andAbove), null);
 		
 		return rd;
 	}
@@ -264,6 +293,6 @@ public class DRCFacilityBasedARTDeliveryReportManager extends ActivatedReportMan
 	@Override
 	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
 		return Arrays
-		        .asList(ReportManagerUtil.createCsvReportDesign("637aa0c0-c6ea-435b-a43d-21f01fd8af42", reportDefinition));
+		        .asList(ReportManagerUtil.createCsvReportDesign("c68114a8-5a61-4e58-a07d-e69f75cde5a6", reportDefinition));
 	}
 }
